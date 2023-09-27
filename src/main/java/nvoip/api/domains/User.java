e463d569bc385a86c1e29e31cc3ecea0814074de
MateusLeviDev <mateuslevisouzapereira@gmail.com>
2023-09-27 18:59:49 -0300

    feat: create simple code example

--- src/main/java/nvoip/api/domains/User.java
+++ src/main/java/nvoip/api/domains/User.java
@@ -1,11 +1,33 @@
 package nvoip.api.domains;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 @Entity
+@Table(name = "users")
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "user_name")
     private String name;
+
+    @Column(name = "sip_number")
     private String numbersip;
+
+    @Column(name = "user_token")
     private String userToken;
+
+    @Column(name = "telephone")
     private String telephone;
 }
