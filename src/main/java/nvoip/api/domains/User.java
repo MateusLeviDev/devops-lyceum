eb4abadbe71dfbfea45138c0f318802b818a04e1
MateusLeviDev <mateuslevisouzapereira@gmail.com>
2023-09-27 18:26:19 -0300

    chore: added new pom dependencies @Lombok @MapStruct @Validation

---
+++ src/main/java/nvoip/api/domains/User.java
@@ -0,0 +1,11 @@
+package nvoip.api.domains;
+
+@Entity
+public class User {
+
+    private Long id;
+    private String name;
+    private String numbersip;
+    private String userToken;
+    private String telephone;
+}
