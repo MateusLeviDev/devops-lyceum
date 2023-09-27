463cfd1c4275436487a2e947b615bca465db4be4
MateusLeviDev <mateuslevisouzapereira@gmail.com>
2023-09-27 16:55:14 -0300

    Initial commit

---
+++ src/main/java/nvoip/api/NvoipApiApplication.java
@@ -0,0 +1,13 @@
+package nvoip.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NvoipApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NvoipApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nvoip/api/NvoipApiApplicationTests.java
@@ -0,0 +1,13 @@
+package nvoip.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NvoipApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
