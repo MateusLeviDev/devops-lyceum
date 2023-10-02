e94b3a07d1ac9a1d13b240f9da33b66e7f779523
MateusLeviDev <mateuslevisouzapereira@gmail.com>
2023-10-02 19:57:41 -0300

    chore: implement `findIUserByNumbersip` e `createMistake`

--- src/main/java/nvoip/api/services/CoreService.java
+++ src/main/java/nvoip/api/services/CoreService.java
@@ -1,6 +1,11 @@
 package nvoip.api.services;
 
 import lombok.RequiredArgsConstructor;
+import nvoip.api.domains.Log;
+import nvoip.api.domains.LogsType;
+import nvoip.api.domains.Mistakes;
+import nvoip.api.domains.User;
+import nvoip.api.exceptions.ElementNotFoundException;
 import nvoip.api.repositories.LogsTypeRepository;
 import nvoip.api.repositories.MistakesRepository;
 import nvoip.api.repositories.UserRepository;
@@ -13,7 +18,20 @@ public class CoreService {
     private final LogsTypeRepository logsTypeRepository;
     private final MistakesRepository mistakesRepository;
 
-    public boolean checkUser(String numbersip) {
-        return this.userRepository.findByNumbersip(numbersip).isPresent();
+    public User findUserByNumbersip(String numbersip) {
+        return this.userRepository.findByNumbersip(numbersip)
+                .orElseThrow(() -> new ElementNotFoundException("Numbersip not found!"));
+    }
+
+    public void createMistake(Integer id, String error) {
+        Mistakes mistakes = new Mistakes(id, error);
+        this.mistakesRepository.save(mistakes);
+    }
+
+    public void createLog(String message, int idUser, String ip, String type) {
+        LogsType logsType = this.logsTypeRepository.findByType(type)
+                .orElseGet(() -> this.logsTypeRepository.save(new LogsType(type)));
+
+        Log log = new Log(idUser, ip, logsType.getId(), message);
     }
 }
