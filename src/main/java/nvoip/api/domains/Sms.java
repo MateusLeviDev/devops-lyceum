b5c1a9b348352c7eb53f32efebe69414179d7651
MateusLeviDev <mateuslevisouzapereira@gmail.com>
2023-10-03 23:47:35 -0300

    feat: create sms e torpedo domains

---
+++ src/main/java/nvoip/api/domains/Sms.java
@@ -0,0 +1,29 @@
+package nvoip.api.domains;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "sms")
+public class Sms {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "message")
+    private String message;
+
+    @Column(name = "sender")
+    private String sender;
+
+    @Column(name = "destiny")
+    private String destiny;
+}
