ed04ae3bb790ff0a6ee3473c5c529912d09c0c7a
MateusLeviDev <mateuslevisouzapereira@gmail.com>
2023-10-04 15:17:47 -0300

    chore: change user type

--- src/main/java/nvoip/api/repositories/UserRepository.java
+++ src/main/java/nvoip/api/repositories/UserRepository.java
@@ -7,8 +7,9 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Integer> {
     Optional<User> findByNumbersip(String numbersip);
     Optional<User> findIdByNumbersip(String numbersip);
     Optional<User> deleteByNumbersip(String numbersip);
+    boolean existsByEmail(String email);
 }
